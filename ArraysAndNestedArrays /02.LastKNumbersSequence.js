3a18b77a4b3cb68d5b40666be314c8463a6b15be
PlamenaKaloyanova <114017277+PlamenaKaloyanova@users.noreply.github.com>
2023-08-09 08:09:35 +0300

    Create 02.LastKNumbersSequence.js

---
+++ ArraysAndNestedArrays /02.LastKNumbersSequence.js
@@ -0,0 +1,16 @@
+function last(n,k) {
+
+let array=[1]
+
+    for (let i = 1; i < n; i++) {
+        let sum = 0;
+        let startIndex = Math.max(0, i - k);
+
+        for (let j = startIndex; j < i; j++) {
+            sum += array[j];
+        }
+        array[i] = sum;
+    }
+    return array;
+
+}
