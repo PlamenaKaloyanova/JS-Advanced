086d7de697ffd5ea4f2291e2bb5cf3da04c68fd1
PlamenaKaloyanova <114017277+PlamenaKaloyanova@users.noreply.github.com>
2023-08-09 08:50:54 +0300

    Create 03.sumNumbers.js

---
+++ DOMIntroduction/03.sumNumbers.js
@@ -0,0 +1,12 @@
+function calc() {
+   
+let num1=document.getElementById('num1').value
+let num2=document.getElementById('num2').value
+
+let sum=document.getElementById('sum');
+
+let sumResult=Number(num1)+Number(num2);
+
+sum.value=sumResult
+
+}
