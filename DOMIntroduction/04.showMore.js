3597b36333ba0dce77ea805106312e7882093689
PlamenaKaloyanova <114017277+PlamenaKaloyanova@users.noreply.github.com>
2023-08-09 08:51:21 +0300

    Create 04.showMore.js

---
+++ DOMIntroduction/04.showMore.js
@@ -0,0 +1,9 @@
+function showText() {
+ 
+let moreElement=document.getElementById('more')
+moreElement.style.display='none';
+
+let text=document.getElementById('text');
+text.style.display='inline';
+
+}
